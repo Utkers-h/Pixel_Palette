7d7da9657ef76654d59e8cb56770cc352918906e
Utkersh uppal <utkersh2003uppal@gmail.com>
2024-03-26 13:47:11 +0530

    Implementing clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export default authMiddleware({
   // publicRoutes: ['/anyone-can-visit-this-route'],
   // Prevent the specified routes from accessing
   // authentication information:
-  publicRoutes: ["/"],
+  publicRoutes: ["/","/api/webhooks/clerk"],
     
   
 
